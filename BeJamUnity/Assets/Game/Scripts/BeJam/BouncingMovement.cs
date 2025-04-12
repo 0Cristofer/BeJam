8c5530e768c86f52aa2807f54a4094efd9457e10
Cristofer Oswald <cristoferoswald@gmail.com>
2025-04-12 15:39:26 -0400

    Basic bouncing movement

---
+++ BeJamUnity/Assets/Game/Scripts/BeJam/BouncingMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace BeJam
+{
+    public class BouncingMovement : MonoBehaviour
+    {
+        [field: SerializeField]
+        private Rigidbody2D Rigidbody2D { get; set; }
+
+        [field: SerializeField] 
+        private Vector2 StartVelocity { get; set; }
+        
+        [field: SerializeField] 
+        private Vector2 CurrentVelocity { get; set; }
+
+        private void Start()
+        {
+            CurrentVelocity = StartVelocity;
+            Rigidbody2D.linearVelocity = CurrentVelocity;
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.transform.position.x > 0)
+            {
+                CurrentVelocity = new Vector2(-CurrentVelocity.x, CurrentVelocity.y);
+            }
+            else if (other.gameObject.transform.position.x < 0)
+            {
+                CurrentVelocity = new Vector2(-CurrentVelocity.x, CurrentVelocity.y);
+            }
+            else if (other.gameObject.transform.position.y > 0)
+            {
+                CurrentVelocity = new Vector2(CurrentVelocity.x, -CurrentVelocity.y);
+            }
+            else if (other.gameObject.transform.position.y < 0)
+            {
+                CurrentVelocity = new Vector2(CurrentVelocity.x, -CurrentVelocity.y);
+            }
+            
+            Rigidbody2D.linearVelocity = CurrentVelocity;
+        }
+    }
+}
--- BeJamUnity/Assets/Game/Scripts/BeJam/PlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace BeJam
-{
-    public class PlayerMovement : MonoBehaviour
-    {
-        
-    }
-}
